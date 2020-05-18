acb9f3d0c9bfe7e25d0f1282cbc899ff7dc669f4
Matheus Morais <morais629@gmail.com>
2020-05-18 15:26:24 -0300

    Criando base de dados H2 + criando ID para minha classe Projetos

--- src/main/java/com/apirest/backprojeto/models/Projeto.java
+++ src/main/java/com/apirest/backprojeto/models/Projeto.java
@@ -2,12 +2,16 @@ package com.apirest.backprojeto.models;
 
 import java.io.Serializable;
 
+import javax.annotation.Generated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 public class Projeto implements Serializable{
     private static final long serialVersionUID = 1L;
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String titulo;
