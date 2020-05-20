83c23ac36ed6c8e772f8005961a67fe2f5cc797a
Matheus Morais <morais629@gmail.com>
2020-05-20 18:51:03 -0300

    Paginação utilizando Page

--- src/main/java/com/apirest/backprojeto/resources/ProjetoResource.java
+++ src/main/java/com/apirest/backprojeto/resources/ProjetoResource.java
@@ -9,11 +9,13 @@ import com.apirest.backprojeto.models.Projeto;
 import com.apirest.backprojeto.services.ProjetoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -68,5 +70,21 @@ public class ProjetoResource {
         return ResponseEntity.ok().body(listDTO);
 
     } 
+
+    @RequestMapping(value="/page", method=RequestMethod.GET)
+    public ResponseEntity<Page<ProjetoDTO>> findPage(
+           @RequestParam(value="page", defaultValue = "0") Integer page, 
+           @RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage,   
+           @RequestParam(value="orderBy", defaultValue = "titulo") String orderBy, 
+           @RequestParam(value="direction", defaultValue = "ASC") String direction    ) {
+
+        Page<Projeto> list = service.findPage(page,linesPerPage,orderBy,direction);
+        Page<ProjetoDTO> listDTO = list.map(obj -> new ProjetoDTO(obj));
+
+        return ResponseEntity.ok().body(listDTO);
+
+    } 
+
+
 
 }
--- src/main/java/com/apirest/backprojeto/services/ProjetoService.java
+++ src/main/java/com/apirest/backprojeto/services/ProjetoService.java
@@ -3,13 +3,17 @@ package com.apirest.backprojeto.services;
 import java.util.List;
 import java.util.Optional;
 
-import com.apirest.backprojeto.models.Projeto;
-import com.apirest.backprojeto.repositories.ProjetoRepository;
 import com.apirest.backprojeto.services.exception.DataIntegrityException;
 import com.apirest.backprojeto.services.exception.ObjectNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import com.apirest.backprojeto.models.Projeto;
+import com.apirest.backprojeto.repositories.ProjetoRepository;
 
 @Service
 public class ProjetoService {
@@ -23,7 +27,7 @@ public class ProjetoService {
                     "Objeto não encontrado! Id: " + id + ", Tipo: " + Projeto.class.getName())); 
 
     }
-
+    
     public List<Projeto> findAll(){    
         return repo.findAll();
     }
@@ -46,8 +50,12 @@ public class ProjetoService {
             repo.deleteById(id);
         } 
         catch (DataIntegrityViolationException e) {
-            throw new DataIntegrityException("Não é possivel excluir uma Categoria que possui Produtos"); 
+            throw new DataIntegrityException("Não é possivel excluir uma Projeto que possui Pessoas Envolvidas"); 
         }
     }
+    public Page<Projeto> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
 
 }
